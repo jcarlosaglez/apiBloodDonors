7c9d5b6de07355337ca4b1a1c79e19590c3d065e
Juan Carlos Andres Gonzalez <jcarlosaglezzz@gmail.com>
2021-02-26 10:53:53 -0600

    Creando el modelo que representa a un receptor.

---
+++ models/Receiver.js
@@ -0,0 +1,15 @@
+// Receiver.js
+// Clase que representa a un receptor
+
+class Receiver {
+	constructor(id, curp, firstName, lastName, dateOfBirth, placeOfResidence, email, phoneNumber) {
+		this.id = id;
+		this.curp = curp;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dateOfBirth = dateOfBirth;
+		this.placeOfResidence = placeOfResidence;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+	}
+}
