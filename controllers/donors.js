1afc0ce30a5a2a0829166d09685cbda613e817dd
Juan Carlos Andres Gonzalez <jcarlosaglezzz@gmail.com>
2021-02-28 09:48:03 -0600

    Ajustes a los controladores.

--- controllers/donors.js
+++ controllers/donors.js
@@ -6,18 +6,18 @@ function createDonador(req, res) {
 }
 
 function readDonadores(req, res) {
-	var donador1 = new Donor(1, 'MAVSJ1312928DDCDDFAS', 'Juan', 'Lopez', '13/12/1995', 'Masculino', 'lp12@gamil.com', '5527388901', 'CDMX', 'A positivo', 'archivo.pdf', forms)
-	var donador2 = new Donor(2, 'MAVSJ1312928DDCDDFAS', 'Maria', 'Dominguez', '05/01/1994', 'Femenino', 'mdda@gamil.com', '5527388889', 'CDMX', 'A negativo', 'archivo.pdf', forms)
+	var donador1 = new Donor(1, 'MAVSJ1312928DDCDDFAS', 'Juan', 'Lopez', '13/12/1995', 'Masculino', 'lp12@gamil.com', '5527388901', 'CDMX', 'A positivo', 'archivo.pdf', "forms");
+	var donador2 = new Donor(2, 'MAVSJ1312928DDCDDFAS', 'Maria', 'Dominguez', '05/01/1994', 'Femenino', 'mdda@gamil.com', '5527388889', 'CDMX', 'A negativo', 'archivo.pdf', "forms");
 	res.send([donador1, donador2])
 }
 
 function readDonador(req, res) {
-	var donador1 = new Donor(req.params.id , 'MAVSJ1312928DDCDDFAS', 'Juan', 'Lopez', '13/12/1995', 'Masculino', 'lp12@gamil.com', '5527388901', 'CDMX', 'A positivo', 'archivo.pdf', forms)
+	var donador1 = new Donor(req.params.id , 'MAVSJ1312928DDCDDFAS', 'Juan', 'Lopez', '13/12/1995', 'Masculino', 'lp12@gamil.com', '5527388901', 'CDMX', 'A positivo', 'archivo.pdf', "forms")
 	res.send([donador1])
 }
 
 function updateDonador(req, res) {
-	var donador1 = new Donor(1, 'MAVSJ1312928DDCDDFAS', 'Juan', 'Lopez', '13/12/1995', 'Masculino', 'l990@gamil.com', '5527388901', 'CDMX', 'A positivo', 'archivo.pdf', forms)
+	var donador1 = new Donor(1, 'MAVSJ1312928DDCDDFAS', 'Juan', 'Lopez', '13/12/1995', 'Masculino', 'l990@gamil.com', '5527388901', 'CDMX', 'A positivo', 'archivo.pdf', "forms")
 	var modificaciones = req.body
 	donador1 = { ...donador1, ...modificaciones }
 	res.send(donador1)
