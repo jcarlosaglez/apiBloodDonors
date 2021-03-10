62bdae78d9d47eaad79627a1846355cb9e8c6a9d
Juan Carlos Andres Gonzalez <jcarlosaglezzz@gmail.com>
2021-03-10 13:30:09 -0600

    Cambios menores.

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(bodyParser.json());
 // Configurar mongoose
 const mongoose = require("mongoose");
 
-const connection_string = require("config/mongo.js").connection_string;
+const connection_string = require("./config/mongo.js").connection_string;
 
 /* mongoose.connect(
     connection_string,
