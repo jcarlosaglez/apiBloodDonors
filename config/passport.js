381d87178be6942afb21d27d09faca88e477e964
Juan Carlos Andres Gonzalez <jcarlosaglezzz@gmail.com>
2021-03-10 12:47:56 -0600

    Modificando passport para dos tipos de autenticaciones.

--- config/passport.js
+++ config/passport.js
@@ -1,19 +1,35 @@
 const passport = require("passport");
-const localStrategy = require("passport-local").Strategy;
+const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Donor = mongoose.model("Donor");
+const Receiver = mongoose.model("Receiver");
 
-passport.use(new localStrategy({
+passport.use("local-donor", new LocalStrategy({
 		usernameField: "email",
 		passwordField: "password"
 	}, function (email, password, done) {
-		Donor
-		.findOne({email: email})
-		.then(function (user) {
-			if(!user || !user.validatePassword(password)) {
-				return done(null, false, {errors: {"email o contraseña": "No coinciden"}});
-			}
-			return done(null, user);
-		})
-		.catch(done);
-}));
+		Donor.findOne({email: email})
+			.then(function (user) {
+				if(!user || !user.validatePassword(password)) {
+					return done(null, false, {errors: {"email o contraseña": "No coinciden"}});
+				}
+				return done(null, user);
+			})
+			.catch(done);
+	}
+));
+
+passport.use("local-receiver", new LocalStrategy({
+		usernameField: "email",
+		passwordField: "password"
+	}, function (email, password, done) {
+			Receiver.findOne({email: email})
+			.then(function (user) {
+				if(!user || !user.validatePassword(password)) {
+					return done(null, false, {errors: {"email o contraseña": "No coinciden"}});
+				}
+				return done(null, user);
+			})
+			.catch(done);
+	}
+));
