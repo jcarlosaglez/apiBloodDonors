f2317184e976fe13007efa06d1b1c044f53a45db
Juan Carlos Andres Gonzalez <jcarlosaglezzz@gmail.com>
2021-03-11 19:10:13 -0600

    Cambios menores.

--- config/index.js
+++ config/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-	secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret'
+	secret: process.env.NODE_ENV === 'production' ? process.env.SECRET : 'secret',
+	mongoDebug: process.env.NODE_ENV === 'production' ? false : true
 };
