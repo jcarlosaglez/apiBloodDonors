5889626d461ea8b5a353db1d2a7b528039ea61cd
Juan Carlos Andres Gonzalez <jcarlosaglezzz@gmail.com>
2021-03-12 23:53:10 -0600

    Añadiendo ruta para paginacion.

--- routes/receivers.js
+++ routes/receivers.js
@@ -27,6 +27,7 @@ var auth = require("./auth");
 
 router.post("/", createReceiver);
 router.get("/", auth.optional, readReceiver);
+router.get("/pagination", auth.optional, readReceiver);
 router.get("/:id", auth.optional, readReceiver);
 router.put("/:id", auth.required, updateReceiver);
 router.delete("/", auth.required, deleteReceiver);
